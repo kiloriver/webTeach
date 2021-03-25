200a2295edaeb99207b17687d71ba6a6b8bbdd1b
unknown <1121429732@qq.com>
2021-03-25 15:33:46 +0800

    fixed multi rooms video play

--- router/student/offer.js
+++ router/student/offer.js
@@ -3,33 +3,34 @@
 //const { broadcaster } = require('../teacher/offer')
 //var broadcaster = null;
 const logger = require('../../lib/logger').logger('student-offer');
+
 function beforeOffer(peerConnection,options) {
-  logger.debug('viewer server | beforeOffer enter options:%o',options);
+  logger.debug('viewer server | beforeOffer enter');
   const broadcaster = options.broadcaster;
   const lesson = options.lesson;
-
+  logger.debug('beforeOffer | lesson:%s and broadcast:%o',lesson,broadcaster);
   const audioTransceiver = peerConnection.addTransceiver('audio');
   const videoTransceiver = peerConnection.addTransceiver('video');
   
   function onNewBroadcast({ audioTrack, videoTrack }) {
     logger.debug('onNewBroadcast enter broadcaster.lesson:%s lesson:%s',broadcaster.lesson,lesson);
-    if(broadcaster.lesson !== lesson) return;
+    //if(broadcaster.lesson !== lesson) return;
     
     audioTransceiver.sender.replaceTrack(audioTrack),
     videoTransceiver.sender.replaceTrack(videoTrack) 
   }
-  logger.debug('viewer server | beforeOffer : begin on %s  %o',lesson,broadcaster);
-  broadcaster.on(lesson, onNewBroadcast);
+  logger.debug('beforeOffer on | %s  %s',lesson,broadcaster.lesson);
+  broadcaster.on("lesson", onNewBroadcast);
 
   if (broadcaster.audioTrack && broadcaster.videoTrack) {
-    logger.debug('viewer server | beforeOffer : begin onNewBroadcast');
+    logger.debug('beforeOffer onNewBroadcast');
     onNewBroadcast(broadcaster);
   }
 
   const { close } = peerConnection;
   peerConnection.close = function() {
     logger.debug('viewer client | beforeOffer : begin removeListener');
-    broadcaster.removeListener('newBroadcast', onNewBroadcast);
+    broadcaster.removeListener("lesson", onNewBroadcast);
     return close.apply(this, arguments);
   }
 }
--- router/teacher/offer.js
+++ router/teacher/offer.js
@@ -2,16 +2,16 @@
 
 const { EventEmitter } = require('events');
 const logger = require('../../lib/logger').logger('teacher-offer');
-// let broadcaster = null;//new EventEmitter();
+
 function beforeOffer(peerConnection,options) {
-  logger.debug('broadcaster server | beforeOffer enter ');
   const broadcaster = options.broadcaster;
   const lesson = options.broadcaster.lesson;
-  
+  logger.debug('beforeOffer | lesson:%s and broadcast:%o',lesson,broadcaster);
   const audioTrack = broadcaster.audioTrack = peerConnection.addTransceiver('audio').receiver.track;
   const videoTrack = broadcaster.videoTrack = peerConnection.addTransceiver('video').receiver.track;
   logger.debug('broadcaster server | beforeOffer :broadcaster.emit %s',lesson);
-  broadcaster.emit('lesson', {
+  
+  broadcaster.emit("lesson", {
     audioTrack,
     videoTrack
   });
